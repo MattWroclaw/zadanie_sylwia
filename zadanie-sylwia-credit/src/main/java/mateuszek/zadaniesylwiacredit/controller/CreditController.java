cee371a97e4e4ffc7f1fd8c11e4ea56ce1dd7dec
Mateusz <whiteelk@wp.pl>
2020-02-23 23:34:22 +0100

    Fixing things

--- zadanie-sylwia-credit/src/main/java/mateuszek/zadaniesylwiacredit/controller/CreditController.java
+++ zadanie-sylwia-credit/src/main/java/mateuszek/zadaniesylwiacredit/controller/CreditController.java
@@ -22,58 +22,64 @@ public class CreditController {
     private final CreditRepository repository;
     private final CreditService creditService;
 
+//    @PostMapping("/createcredit")
+//    public int createCredit(@RequestBody final Credit credit) {
+//
+//        Credit savedCredit = creditService.saveCredit(credit);
+//        int creditId = savedCredit.getId(); // get creditId from newly saved object credit
+//        savedCredit.getCustomer().setCredtiId(creditId); // from the credit get the customer, set the field creditId with creditId taken from above
+//        restTemplate.postForObject("http://localhost:8300/customer/addcustomer",
+//                credit.getCustomer(), Customer.class); // pass customer to other service where it will be saved id DB
+//
+//        savedCredit.getProduct().setCreditId(creditId);
+//        restTemplate.postForObject("http://localhost:8302/product/addproduct",
+//                credit.getProduct(), Product.class);
+//        return creditId;
+//    }
+
     @PostMapping("/createcredit")
     public int createCredit(@RequestBody final Credit credit) {
 
         Credit savedCredit = creditService.saveCredit(credit);
         int creditId = savedCredit.getId(); // get creditId from newly saved object credit
         savedCredit.getCustomer().setCredtiId(creditId); // from the credit get the customer, set the field creditId with creditId taken from above
-        restTemplate.postForObject("http://localhost:8300/customer/addcustomer",
+        restTemplate.postForObject("http://customer:8300/customer/addcustomer",
                 credit.getCustomer(), Customer.class); // pass customer to other service where it will be saved id DB
 
         savedCredit.getProduct().setCreditId(creditId);
-        restTemplate.postForObject("http://localhost:8302/product/addproduct",
+        restTemplate.postForObject("http://product:8302/product/addproduct",
                 credit.getProduct(), Product.class);
         return creditId;
     }
 
+//
     @GetMapping("/getcredits")
     public List<Credit> getCredits() {
         List<Credit> creditList = repository.findAll(); // we take all the credits
-        List<Credit> returnedList = new ArrayList<>(); // this list will be used to store required objects
-        for (int i = 0; i < creditList.size(); i++) { // we iterate over the list of credits..
-            Credit singleCredit = creditList.get(i); // ..for each iteration take a single credit..
-            int creditId = singleCredit.getId(); // ... from single credit we get to know the id of it
-
-
-//            **********************************************************************************************
-//            Customer customer = restTemplate.exchange("http://localhost:8300/customer/id=" + creditId,
-//                    HttpMethod.GET, null, new ParameterizedTypeReference<Customer>() {
-//                    }).getBody(); // we pass query to other service to find that (here) customer by credit id
-//            singleCredit.setCustomer(customer); // set received object (here customer) to appropriate credit..
-//
-//            // We do the same to product
-//            Product product = restTemplate.exchange("http://localhost:8302/product/id=" + creditId,
-//                    HttpMethod.GET, null, new ParameterizedTypeReference<Product>() {
-//                    }).getBody();
-//            singleCredit.setProduct(product);
-//      *************************************************************************************************
-
-            Customer customer = restTemplate.exchange("http://customer:8300/customer/id=" + creditId,
-                    HttpMethod.GET, null, new ParameterizedTypeReference<Customer>() {
-                    }).getBody(); // we pass query to other service to find that (here) customer by credit id
-            singleCredit.setCustomer(customer); // set received object (here customer) to appropriate credit..
-
-            // We do the same to product
-            Product product = restTemplate.exchange("http://product:8302/product/id=" + creditId,
-                    HttpMethod.GET, null, new ParameterizedTypeReference<Product>() {
-                    }).getBody();
-            singleCredit.setProduct(product);
-
-//            ********************************************************************
-
-            returnedList.add(singleCredit); // add to the collective list that will be returned
-        }
+        creditList.forEach(e -> {
+            Customer customer = restTemplate.exchange("http://customer:8300/customer/id=" + e.getId(),
+                    HttpMethod.GET, null, Customer.class).getBody();
+            Product product = restTemplate.exchange("http://product:8302/product/id=" + e.getId(),
+                    HttpMethod.GET, null, Product.class).getBody();
+            e.setCustomer(customer);
+            e.setProduct(product);
+        });
         return creditList;
     }
+
+//    @GetMapping("/getcredits")
+//    public List<Credit> getCredits() {
+//        List<Credit> creditList = repository.findAll(); // we take all the credits
+//        creditList.forEach(e -> {
+//            Customer customer = restTemplate.exchange("http://localhost:8300/customer/id=" + e.getId(),
+//                    HttpMethod.GET, null, Customer.class).getBody();
+//            Product product = restTemplate.exchange("http://localhost:8302/product/id=" + e.getId(),
+//                    HttpMethod.GET, null, Product.class).getBody();
+//            e.setCustomer(customer);
+//            e.setProduct(product);
+//        });
+//        return creditList;
+//    }
+
+
 }
